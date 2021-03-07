6ca70383abceffca0fa053afc00bc938ab833b43
piotrrussw <piotr.wadycki@gmail.com>
2021-03-07 19:52:26 +0100

    Updates

---
+++ precache-manifest.bbd7c680041364cfd6f191e72e2e6cf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbebd9d0e91bd17619f7240bf50d892",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd3558458c0e8af0ad0e",
+    "url": "./static/css/main.818565c8.chunk.css"
+  },
+  {
+    "revision": "e8f679d53901132ed4e5",
+    "url": "./static/js/2.714204fb.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.714204fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3558458c0e8af0ad0e",
+    "url": "./static/js/main.89821b88.chunk.js"
+  },
+  {
+    "revision": "6d426309fe550b33d5de",
+    "url": "./static/js/runtime-main.dca6af25.js"
+  }
+]);
